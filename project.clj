cd4b0cdde5d5b7fb621c3971e5241b23962d48d2
Howard Abrams <howard.abrams@gmail.com>
2016-05-31 20:33:31 -0700

    Edited the README to be more clear.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rpn-calc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A streaming calculator that computes in reverse polish notation."
+  :url "https://github.com/howardabrams/rpn-calc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
